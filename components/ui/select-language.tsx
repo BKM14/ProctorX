4282432410573bb08839e4a4e6d6ab72439a2024
Balaji Krishnamurthy <balajikrishnamurthy2004@gmail.com>
2025-04-13 23:58:09 +0530

    boilerplate code appears when user selects a language

--- components/ui/select-language.tsx
+++ components/ui/select-language.tsx
@@ -11,13 +11,19 @@ interface LanguageProps {
     label: string
 }
 
-export default function SelectLanguage({languages, setNewLanguage}: {
+export default function SelectLanguage({languages, setNewLanguage, setBoilerPlateCode, boilerPlate}: {
     languages: LanguageProps[]
-    setNewLanguage: (newLanguage: string) => void}) {
+    setNewLanguage: (newLanguage: "java" | "c" | "cpp" | "py") => void
+    setBoilerPlateCode: (lang: string) => void
+    boilerPlate: {
+        "py": string, "java": string, "c": string, "cpp": string
+    }
+    }) {
 
     return (
     <Select onValueChange={(newLanguage: string | undefined) => {
-        setNewLanguage(newLanguage || "py")
+        setNewLanguage((newLanguage as "java" | "c" | "cpp" | "py") || "py")
+        setBoilerPlateCode(boilerPlate[newLanguage as "java" | "c" | "cpp" | "py"])
     }}>
         <SelectTrigger className="w-1/6 m-4">
             <SelectValue placeholder="Python" />
